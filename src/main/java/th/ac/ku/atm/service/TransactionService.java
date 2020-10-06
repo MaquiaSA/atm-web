4ab781f6677def965fa08b076dc4ad0d5b9e8398
Anant Arayanant <minecraftraguna@gmail.com>
2020-10-06 15:17:44 +0700

    use transaction instead of direct edit

---
+++ src/main/java/th/ac/ku/atm/service/TransactionService.java
@@ -0,0 +1,19 @@
+package th.ac.ku.atm.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import th.ac.ku.atm.model.Transaction;
+
+@Service
+public class TransactionService {
+    private RestTemplate restTemplate;
+
+    public TransactionService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public void makeTransaction(Transaction transaction) {
+        String url = "http://localhost:8091/api/bankaccount/transaction";
+        restTemplate.postForObject(url, transaction, Transaction.class);
+    }
+}
